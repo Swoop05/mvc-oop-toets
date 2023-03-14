e2b92b2f627bb25ae190b6e965b9bc705970b957
Steff Brouwer <sbrouwer05@gmail.com>
2023-03-14 11:14:58 +0100

    3de commit

---
+++ app/models/ZangeresModel.php
@@ -0,0 +1,27 @@
+<?php
+
+class CountryModel
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function getCountries()
+    {
+        $sql = 'SELECT  Id
+                       ,Naam
+                       ,NettoWaarde
+                       ,Land
+                       ,Mobiel
+                       ,Leeftijd
+                FROM    Zangeres';
+
+        $this->db->query($sql);
+
+        return $this->db->resultSet();        
+    }
+
+}
